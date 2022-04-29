ccd8b638f5f9ae839853ab4d19fe95a983879161
FelipeRodriguesDeveloper <feliperodriguesdeveloper@hotmail.com>
2022-04-29 05:09:18 -0300

    Adicionado CORS

--- LivrariaAPI/Startup.cs
+++ LivrariaAPI/Startup.cs
@@ -28,6 +28,9 @@ namespace LivrariaAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddDefaultPolicy(
+                       politicaCorsDefault => { politicaCorsDefault.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
+
             services.AddControllers();
 
             services.AddDbContext<LivrariaDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConexaoBancoLivrariaAPI_Azure"), b => b.MigrationsAssembly("Livraria.Data.EFCore")));
@@ -59,7 +62,6 @@ namespace LivrariaAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LivrariaAPI v1"));
@@ -68,6 +70,8 @@ namespace LivrariaAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
