825e78dc67d3f0ba358a820912f61e4780146fb7
FelipeRodriguesDeveloper <feliperodriguesdeveloper@hotmail.com>
2022-05-12 11:24:25 -0300

    Ajustes Swagger

--- LivrariaAPI/Startup.cs
+++ LivrariaAPI/Startup.cs
@@ -62,11 +62,13 @@ namespace LivrariaAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LivrariaAPI v1"));
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LivrariaAPI v1"));
+            }
             
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
